6969a23fbd01c5022bce3750b3d7289299b0dd45
Avinash Agrawal <115834175+Avinash0308@users.noreply.github.com>
2023-12-21 12:54:42 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/compat/firestore';
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCD2_JRLls4x8MctbPex3lbFs2HXq86aUQ",
-    authDomain: "netflix-clone-3777a.firebaseapp.com",
-    projectId: "netflix-clone-3777a",
-    storageBucket: "netflix-clone-3777a.appspot.com",
-    messagingSenderId: "833883179597",
-    appId: "1:833883179597:web:996a2796dddde16feb2a04",
-    measurementId: "G-KQP9Y5N3S4"
-  };
+  apiKey: "AIzaSyC4QZUXRO904g4SOhVZfFfGFvj9d1Q21DI",
+  authDomain: "tumin-0308.firebaseapp.com",
+  projectId: "tumin-0308",
+  storageBucket: "tumin-0308.appspot.com",
+  messagingSenderId: "1008714146714",
+  appId: "1:1008714146714:web:670b07c01ad7a5a7435023",
+  measurementId: "G-991KRSLVTW"
+};
 
   const firebaseApp = firebase.initializeApp(firebaseConfig)
   const db = firebaseApp.firestore();
